74f1d180e46dc40e698a36b08d28f3ac710f6fe8
Walid-Mahmoudi <walid.a.mahmodi@gmail.com>
2023-11-14 16:41:44 +0200

    finsh product detals and privacy policy

---
+++ assets/js/pages/product details.js
@@ -0,0 +1,10 @@
+//------------ Add-Review ------------//
+let btnAddreview = document.getElementById("add-review");
+let openAddreview = document.getElementById("review-input");
+function addReview() {
+    openAddreview.style.display="block"
+}
+
+
+// ///////
+
