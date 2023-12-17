731fa2b20bc87193f67608e063a46b92dff32817
Walid-Mahmoudi <walid.a.mahmodi@gmail.com>
2023-12-17 16:32:23 +0200

    stepeers

--- assets/js/pages/Checkout.js
+++ assets/js/pages/Checkout.js
@@ -1,28 +1,109 @@
 let btnPrevious = document.querySelectorAll(".previous");
-// let btnNext = document.querySelectorAll(".next");
+let btnNext = document.querySelectorAll(".next");
 let nextButton = document.querySelector(".btn-next");
+let backButton = document.querySelector(".back-step");
 let formStep = document.querySelectorAll(".form-step");
-let active =1 ;
-console.log(btnNext);
+let stepsNumber = document.querySelector(".step");
+let titleHeader = document.querySelector(".title-header");
+let progresBar = document.querySelector(".circular-progres");
+let progresValue = document.querySelector(".value-progres");
+let placeOrder = document.querySelector(".place-oreder");
 
-nextButton.addEventListener("click" , ()=>{
-  active++
+let progresValues = 1;
+let progresEndvalues = formStep.length;
+progresBar.style.background = `conic-gradient(#DCB996 ${
+  progresValues * 120
+}deg,#EDE3D9 ${progresValues * 120}deg)`;
+progresValue.textContent = `0${progresValues} of 0${progresEndvalues}`;
+stepsNumber.innerHTML = `${progresValues}`;
+formStep[2].classList.add("active");
+formStep[0].classList.remove("active");
+formStep[1].classList.add("active");
+titleHeader.innerHTML = "Shipping Information";
+
+
+for (const next of btnNext) {
+  next.addEventListener("click", () => {
+    if (progresValues == progresEndvalues) {
+      return false;
+    } else {
+      progresValues++;
+      progresBar.style.background = `conic-gradient(#DCB996 ${
+        progresValues * 120
+      }deg,#EDE3D9 ${progresValues * 120}deg)`;
+      progresValue.textContent = `0${progresValues} of 0${progresEndvalues}`;
+      stepsNumber.innerHTML = `${progresValues}`;
+      if (progresValues == 2) {
+        formStep[2].classList.add("active");
+        formStep[0].classList.add("active");
+        formStep[1].classList.remove("active");
+        titleHeader.innerHTML = "Payment Method";
+        placeOrder.classList.add("disabeld");
+      } else if (progresValues == 3) {
+        formStep[2].classList.remove("active");
+        formStep[0].classList.add("active");
+        formStep[1].classList.add("active");
+        titleHeader.innerHTML = "Review Order";
+        placeOrder.classList.remove("disabeld");
+      }
+    }
+  });
+}
+
+for (const previos of btnPrevious) {
+  previos.addEventListener("click", () => {
+    if (progresValues == 1) {
+      return false;
+    } else {
+      progresValues--;
+      progresBar.style.background = `conic-gradient(#DCB996 ${
+        progresValues * 120
+      }deg,#EDE3D9 ${progresValues * 120}deg)`;
+      progresValue.textContent = `0${progresValues} of 0${progresEndvalues}`;
+      stepsNumber.innerHTML = `${progresValues}`;
+      if (progresValues == 1) {
+        formStep[2].classList.add("active");
+        formStep[0].classList.remove("active");
+        formStep[1].classList.add("active");
+        titleHeader.innerHTML = "Shipping Information";
+      } else if (progresValues == 2) {
+        formStep[2].classList.add("active");
+        formStep[0].classList.add("active");
+        formStep[1].classList.remove("active");
+        titleHeader.innerHTML = "Payment Method";
+        placeOrder.classList.add("disabeld");
+      }
+    }
+  });
+}
+
+
+nextButton.addEventListener("click", ()=>{
+  progresValues++;
+  progresBar.style.background = `conic-gradient(#DCB996 ${
+    progresValues * 120
+  }deg,#EDE3D9 ${progresValues * 120}deg)`;
+  progresValue.textContent = `0${progresValues} of 0${progresEndvalues}`;
+  stepsNumber.innerHTML = `${progresValues}`;
+
+    formStep[2].classList.add("active");
+    formStep[0].classList.add("active");
+    formStep[1].classList.remove("active");
+    titleHeader.innerHTML = "Payment Method";
 })
 
+console.log(backButton);
 
+backButton.addEventListener("click", ()=>{
+  progresValues--;
+  progresBar.style.background = `conic-gradient(#DCB996 ${
+    progresValues * 120
+  }deg,#EDE3D9 ${progresValues * 120}deg)`;
+  progresValue.textContent = `0${progresValues} of 0${progresEndvalues}`;
+  stepsNumber.innerHTML = `${progresValues}`;
 
-// Previous
-// for (const previos of btnPrevious) {
-//   previos.addEventListener("click", () => {
-//     previos.style.color = "red";
-//   });
-// }
-
-// for (const next of btnNext) {
-//   next.addEventListener("click", () => {
-//     shippingInfo.classList.add("disable");
-//     paymentMethod.classList.remove("disable");
-//   });
-// }
-
-
+    formStep[2].classList.add("active");
+    formStep[0].classList.remove("active");
+    formStep[1].classList.add("active");
+    titleHeader.innerHTML = "Shipping Information";
+})
--- assets/js/pages/My Account.js
+++ assets/js/pages/My Account.js
@@ -1,27 +1,39 @@
-	
-
 let edit = document.getElementById("edit");
 let updateInformation = document.querySelector(".update-information");
 let accountInformation = document.querySelector(".profile");
 let updatePassword = document.querySelector(".update-password");
 
-
-
-
-function editAccount(){
-    accountInformation.classList.add("disable");
-    updateInformation.classList.remove("disable")
+function editAccount() {
+  accountInformation.classList.add("disable");
+  updateInformation.classList.remove("disable");
 }
-function back(){
-    accountInformation.classList.remove("disable");
-    updateInformation.classList.add("disable")
+function back() {
+  accountInformation.classList.remove("disable");
+  updateInformation.classList.add("disable");
 }
 
-function resetPassword(){
-    accountInformation.classList.add("disable");
-    updatePassword.classList.remove("disable");
+function resetPassword() {
+  accountInformation.classList.add("disable");
+  updatePassword.classList.remove("disable");
 }
-function backReset(){
-    accountInformation.classList.remove("disable");
-    updatePassword.classList.add("disable")
+function backReset() {
+  accountInformation.classList.remove("disable");
+  updatePassword.classList.add("disable");
 }
+
+let order = document.querySelector(".order");
+let ordersVeiw = document.querySelector(".orders-veiw");
+let orderStatus = document.querySelector(".order-status");
+let backOrders = document.querySelector(".back-orders");
+
+orderStatus.classList.add("active");
+order.addEventListener("click", () => {
+  console.log("hero");
+  ordersVeiw.classList.add("active");
+  orderStatus.classList.remove("active");
+});
+
+backOrders.addEventListener("click", () => {
+  ordersVeiw.classList.remove("active");
+  orderStatus.classList.add("active");
+});
