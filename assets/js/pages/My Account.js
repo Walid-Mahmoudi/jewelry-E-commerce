364514f8b4ae567fb32454ac59459a428a095cf7
Walid-Mahmoudi <walid.a.mahmodi@gmail.com>
2023-12-19 14:28:17 +0200

    edit

--- assets/js/pages/My Account.js
+++ assets/js/pages/My Account.js
@@ -1,3 +1,4 @@
+// ******* MY ACOUNT ********//
 let edit = document.getElementById("edit");
 let updateInformation = document.querySelector(".update-information");
 let accountInformation = document.querySelector(".profile");
@@ -20,7 +21,7 @@ function backReset() {
   accountInformation.classList.remove("disable");
   updatePassword.classList.add("disable");
 }
-
+// ******** MY ORDER **********//
 let order = document.querySelector(".order");
 let ordersVeiw = document.querySelector(".orders-veiw");
 let orderStatus = document.querySelector(".order-status");
@@ -37,3 +38,27 @@ backOrders.addEventListener("click", () => {
   ordersVeiw.classList.remove("active");
   orderStatus.classList.add("active");
 });
+// ********** RETURN *************//
+let refundedButton = document.querySelector(".btn-refunded");
+let reviewingButton = document.querySelector(".btn-reviewing");
+let returnsVeiw = document.querySelector(".returns-veiw");
+let refundedItems = document.querySelector(".refunded-items");
+let reviewingItems = document.querySelector(".reviewing-items");
+
+refundedItems.classList.add("disable");
+reviewingItems.classList.add("disable");
+
+refundedButton.addEventListener("click", () => {
+  refundedItems.classList.remove("disable");
+  returnsVeiw.classList.add("disable");
+});
+reviewingButton.addEventListener("click", () => {
+  reviewingItems.classList.remove("disable");
+  returnsVeiw.classList.add("disable");
+});
+
+function back() {
+  returnsVeiw.classList.remove("disable");
+  refundedItems.classList.add("disable");
+  reviewingItems.classList.add("disable");
+}
